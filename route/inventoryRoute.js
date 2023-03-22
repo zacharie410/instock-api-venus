c4bf9079eb194ea497ee1c899650ed22a291ef93
Zacharie Fortin <zacharie410@gmail.com>
2023-03-22 16:37:20 -0400

    fixed-imports

--- route/inventoryRoute.js
+++ route/inventoryRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const inventoryController = require('../controllers/inventoryController');
+const inventoryController = require('../controller/inventoryController');
 
 // GET all inventorys
 router.get('/', inventoryController.getAll);
--- route/warehouseRoute.js
+++ route/warehouseRoute.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const warehouseController = require('../controllers/warehouseController');
+const warehouseController = require('../controller/warehouseController');
 
 // GET all warehouses
 router.get('/', warehouseController.getAll);
